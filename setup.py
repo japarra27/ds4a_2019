062eedaa141cc0bd8fce58db62de59261a3738e8
Jaime Andres Parra Mora <japarra27@ucatolica.edu.co>
2019-10-09 08:40:30 -0500

    Structure configuration of the project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='''This repository contains the material and activities
+        developed in the ds4a program by CorrelationOne and MinTic Colombia.''',
+    author='Jaime Andres Parra Mora',
+    license='MIT',
+)
